1fb7a5e5c3ed8eff9b58d710507ada73722505cd
Gregg Altschul <galtschul@gmail.com>
2013-10-30 12:01:37 +0000

    upgrading to 1.1

--- tweepy/api.py
+++ tweepy/api.py
@@ -18,7 +18,7 @@ class API(object):
     def __init__(self, auth_handler=None,
                  host='api.twitter.com', search_host='search.twitter.com',
                  upload_host='upload.twitter.com',
-                 cache=None, secure=True, api_root='/1.1', search_root='', upload_root='/1',
+                 cache=None, secure=True, api_root='/1.1', search_root='', upload_root='/1.1',
                  retry_count=0, retry_delay=0, retry_errors=None, timeout=60,
                  parser=None, compression=False):
         self.auth = auth_handler
