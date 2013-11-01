175c5c0b70a1aa1dc1dc76392369e98e04ce701c
Gregg Altschul <galtschul@gmail.com>
2013-11-01 23:08:15 +0000

    moving param position

--- tweepy/api.py
+++ tweepy/api.py
@@ -86,7 +86,7 @@ class API(object):
 
     
     """ status/update_with_media """
-    def update_status_with_media(self, filename, *args, **kargs, file_type=None):
+    def update_status_with_media(self, filename, file_type=None, *args, **kargs):
         headers, post_data = API._pack_image(filename, 3072, form_field='media[]', file_type)
         kargs.update({
                 'headers': headers,
